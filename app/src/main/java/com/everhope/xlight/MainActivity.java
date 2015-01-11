b933ba6f75bba48ebfe4dc85ef1a207a377b1cf3
kungstriving <kongxiaoyangsh@foxmail.com>
2015-01-11 12:57:19 +0800

    添加界面测试

--- app/src/main/java/com/everhope/xlight/MainActivity.java
+++ app/src/main/java/com/everhope/xlight/MainActivity.java
@@ -5,12 +5,19 @@ import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.ResultReceiver;
 import android.preference.PreferenceManager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.everhope.xlight.app.AppUtils;
@@ -40,19 +47,63 @@ public class MainActivity extends ActionBarActivity {
 
         //检查是否第一次运行
         if (isVeryFirstLoad()) {
+            Log.i(TAG, "第一次启动运行");
             //是第一次运行
+            //网络侦测网关地址
             setVeryFirstLoad();
 
         }
 
         //连接到网关
-        connectToGate();
+        //connectToGate();
 
-        getWindow().setBackgroundDrawableResource(R.drawable.splash);
+        //getWindow().setBackgroundDrawableResource(R.drawable.splash);
+
+        Log.i(TAG, "启动运行");
         setContentView(R.layout.activity_main);
 
+        //动态加载splash
+        final RelativeLayout mainLayout = (RelativeLayout)findViewById(R.id.main_layout);
+//        relativeLayout.addView(i);
+        final ImageView imageView = new ImageView(MainActivity.this);
+        imageView.setImageResource(R.drawable.splash);
+        RelativeLayout.LayoutParams layoutParams =
+                new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+
+        mainLayout.addView(imageView, layoutParams);
+        //显示loading
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        progressBar.bringToFront();
+        progressBar.setVisibility(ProgressBar.VISIBLE);
+
+        //模拟5秒钟后进入新画面
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //删除splash view 停止loading
+                progressBar.setVisibility(ProgressBar.INVISIBLE);
+                mainLayout.removeView(imageView);
+                //显示actionbar
+                getSupportActionBar().show();
+                handler.removeCallbacks(this);
+            }
+        }, 5000);
+
+        //设置抽屉
+        //setLeftDrawer();
     }
 
+    private void setLeftDrawer() {
+        String[] leftItems = getResources().getStringArray(R.array.left_nav_items);
+        DrawerLayout drawerLayout = (DrawerLayout)findViewById(R.id.main_drawer_layout);
+        ListView leftListView = (ListView)findViewById(R.id.main_left_drawer);
+
+        leftListView.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, leftItems));
+        //leftListView.setOnItemClickListener(new DrawerItemClickListener());
+    }
     /**
      * 连接到网关
      */
@@ -70,24 +121,25 @@ public class MainActivity extends ActionBarActivity {
 
             DataAgent dataAgent = lightApp.getDataAgent();
 
+            Log.i(TAG, "开始网络广播侦测");
             dataAgent.detectGateInLan(this, new ResultReceiver(new Handler()) {
                 @Override
                 protected void onReceiveResult(int resultCode, Bundle resultData) {
                     if (resultCode == Constants.COMMON.RESULT_CODE_OK) {
                         //获取到了网关STA地址
                         String gateStaIP = resultData.getString(Constants.KEYS_PARAMS.GATE_STA_IP);
-
+                        Log.i(TAG, "获取到了网关地址");
                         //使用sta地址直连网关，即直接登录网关
                         logonToGate(gateStaIP);
 
                     } else {
-                        //连接AP网络
-                        switchToAPNetwork();
+
                         //关闭loading 进入AP网络设置界面
                         if (progressBar.isShown()) {
                             progressBar.setVisibility(ProgressBar.INVISIBLE);
                         }
-
+                        //连接AP网络
+                        switchToAPNetwork();
                         Intent intent = new Intent(MainActivity.this, APSetupActivity.class);
                         startActivity(intent);
                     }
@@ -176,16 +228,12 @@ public class MainActivity extends ActionBarActivity {
      */
     private boolean isVeryFirstLoad() {
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        return pref.getBoolean(Constants.SYSTEM_SETTINGS.IS_LOADED_YET, false);
+        return !pref.getBoolean(Constants.SYSTEM_SETTINGS.IS_LOADED_YET, false);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        //显示loading
-        progressBar = (ProgressBar)findViewById(R.id.progressBar);
-        progressBar.setVisibility(ProgressBar.VISIBLE);
-        //开启后台线程
 
     }
 
