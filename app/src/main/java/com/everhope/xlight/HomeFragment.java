f0a1251ff7fa6044d5a7e387dcca64b94d3afd8e
kungstriving <kongxiaoyangsh@foxmail.com>
2015-01-19 22:20:00 +0800

    添加scene_pane 添加灯控制frag基础框架

--- app/src/main/java/com/everhope/xlight/HomeFragment.java
+++ app/src/main/java/com/everhope/xlight/HomeFragment.java
@@ -5,15 +5,19 @@ import android.content.Context;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.GravityCompat;
 import android.util.DisplayMetrics;
 import android.view.Display;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
+import android.widget.TextView;
 
 /**
  * 主页fragment
@@ -56,20 +60,23 @@ public class HomeFragment extends Fragment{
         currentLayoutparams.setMargins(10,10,10,10);
         linearLayout.addView(currentLineLayout, currentLayoutparams);
 
+        //逐个加入scene控制块
         for (int i = 0; i < sceneCount; i++) {
             if (currentLineCount == 3) {
-
                 currentLineLayout = new LinearLayout(getActivity());
                 currentLineLayout.setOrientation(LinearLayout.HORIZONTAL);
-
                 linearLayout.addView(currentLineLayout, currentLayoutparams);
                 currentLineCount = 0;
             }
 
             //add scene to currentlinearlayout
-            Button button = new Button(getActivity());
-            button.setText(i+"");
-            button.setOnClickListener(new View.OnClickListener() {
+//            View scenePane = inflater.inflate(R.layout.view_scene_pane, container);
+            LinearLayout scenePane = new LinearLayout(getActivity());
+            scenePane.setOrientation(LinearLayout.VERTICAL);
+
+            ImageView sceneImg = new ImageView(getActivity());
+            sceneImg.setImageResource(R.drawable.ic_launcher);
+            sceneImg.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     PopupWindow popup = new PopupWindow(getActivity());
@@ -77,18 +84,30 @@ public class HomeFragment extends Fragment{
                     popup.setContentView(layout);
                     // Set content width and height
                     popup.setHeight(WindowManager.LayoutParams.WRAP_CONTENT);
-                    popup.setWidth(WindowManager.LayoutParams.WRAP_CONTENT);
+                    popup.setWidth(WindowManager.LayoutParams.MATCH_PARENT);
                     // Closes the popup window when touch outside of it - when looses focus
                     popup.setOutsideTouchable(true);
                     popup.setFocusable(true);
+
                     // Show anchored to button
                     popup.setBackgroundDrawable(new BitmapDrawable());
                     popup.showAsDropDown(v);
+//                    popup.showAsDropDown(v,0, -300);
                 }
             });
-            button.setBackgroundColor(getResources().getColor(R.color.gray));
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(cellWidth, cellWidth);   //same width height
-            currentLineLayout.addView(button, layoutParams);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(cellWidth-10, cellWidth-10);   //same width height
+            scenePane.addView(sceneImg, layoutParams);
+
+            TextView sceneText = new TextView(getActivity());
+            LinearLayout.LayoutParams textLayoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 50);
+            sceneText.setText("scene");
+            sceneText.setGravity(Gravity.CENTER_HORIZONTAL);
+
+            scenePane.addView(sceneText, textLayoutParams);
+
+            LinearLayout.LayoutParams sceneLayoutParams = new LinearLayout.LayoutParams(cellWidth, cellWidth + 50);
+            sceneLayoutParams.setMargins(5,5,5,5);
+            currentLineLayout.addView(scenePane, sceneLayoutParams);
             currentLineCount++;
         }
 
--- app/src/main/java/com/everhope/xlight/LightFragment.java
+++ app/src/main/java/com/everhope/xlight/LightFragment.java
@@ -1,11 +1,20 @@
 package com.everhope.xlight;
 
 
-import android.os.Bundle;
+import android.app.Activity;
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -14,6 +23,8 @@ import android.view.ViewGroup;
 public class LightFragment extends Fragment {
 
     private static final String ARG_HELLO_LIGHT = "hello_light";
+    private  List<String> groupArray;
+    private  List<List<String>> childArray;
 
     private String mHelloLight;
 
@@ -27,11 +38,38 @@ public class LightFragment extends Fragment {
         Bundle args = new Bundle();
         args.putString(ARG_HELLO_LIGHT, paramHelloLight);
         fragment.setArguments(args);
+
         return fragment;
     }
 
     public LightFragment() {
         // Required empty public constructor
+        groupArray = new ArrayList<>();
+        childArray = new ArrayList<>();
+
+        groupArray.add("卧室");
+        groupArray.add("客厅");
+
+        List<String> tempArray = new ArrayList<>();
+        tempArray.add("00001");
+        tempArray.add("00002");
+        tempArray.add("00003");
+        tempArray.add("00004");
+        tempArray.add("00005");
+        tempArray.add("00006");
+
+        //add 卧室
+        childArray.add(tempArray);
+
+        tempArray = new ArrayList<>();
+        tempArray.add("10001");
+        tempArray.add("10002");
+        tempArray.add("10003");
+        tempArray.add("10004");
+        tempArray.add("10005");
+        tempArray.add("10006");
+
+        childArray.add(tempArray);
     }
 
     @Override
@@ -46,8 +84,88 @@ public class LightFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_light, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_light, container, false);
+
+        ExpandableListView expandableListView = (ExpandableListView)rootView.findViewById(R.id.expandableListView);
+        expandableListView.setAdapter(new LightExpandableListAdapter(getActivity()));
+
+        getActivity().setTitle(R.string.light_fragment_title);
+        return rootView;
     }
 
+    class LightExpandableListAdapter extends BaseExpandableListAdapter {
+
+        Activity activity;
+        public LightExpandableListAdapter(Activity activity) {
+            this.activity = activity;
+        }
+
+        @Override
+        public Object getChild(int groupPosition, int childPosition) {
+            return childArray.get(groupPosition).get(childPosition);
+        }
+
+        @Override
+        public long getChildId(int groupPosition, int childPosition) {
+            return childPosition;
+        }
+
+        @Override
+        public int getChildrenCount(int groupPosition) {
+            return childArray.get(groupPosition).size();
+        }
+
+        @Override
+        public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+            String str = childArray.get(groupPosition).get(childPosition);
+            return generateView(str);
+        }
+
+        @Override
+        public Object getGroup(int groupPosition) {
+            return groupArray.get(groupPosition);
+        }
+
+        @Override
+        public int getGroupCount() {
+            return groupArray.size();
+        }
+
+        @Override
+        public long getGroupId(int groupPosition) {
+            return groupPosition;
+        }
+
+        @Override
+        public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+            String string = groupArray.get(groupPosition);
+            return  generateView(string);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return false;
+        }
+
+        @Override
+        public boolean isChildSelectable(int groupPosition, int childPosition) {
+            return true;
+        }
+
+        public TextView generateView(String string)
+        {
+            // Layout parameters for the ExpandableListView
+            AbsListView.LayoutParams layoutParams = new  AbsListView.LayoutParams(
+                    ViewGroup.LayoutParams.FILL_PARENT, 64 );
+            TextView text = new  TextView(activity);
+            text.setLayoutParams(layoutParams);
+            // Center the text vertically
+            text.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);
+            // Set the text starting position
+            text.setPadding(36 , 0 , 0 , 0 );
+            text.setText(string);
+            return  text;
+        }
+    }
 
 }
